3b1d5169ee383890941a65c7b29a9780174801a9
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-05-23 23:06:35 -0400

    fix steps

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@ try {
 
 // Overwrite the Contentful config with environment variables if they exist
 contentfulConfig = {
-  spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
+  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || contentfulConfig.accessToken,
-  host: process.env.GATSBY_CONTENTFUL_HOST
+  host: process.env.CONTENTFUL_HOST
 }
 
 console.log(contentfulConfig, 'CONFIG')
